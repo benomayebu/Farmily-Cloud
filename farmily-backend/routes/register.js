f677dc9d48443832c5e13f318f39b09634a21385
benomayebu <156213376+benomayebu@users.noreply.github.com>
2024-11-25 03:19:30 +0000

    Update register.js

--- farmily-backend/routes/register.js
+++ farmily-backend/routes/register.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const User = require('../models/User');
+const User = require('../models/user.js');
 
 // Hash password function
 const hashPassword = async (password) => {
